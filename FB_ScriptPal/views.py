eb9ce97eceb48b3eff03f293fecc352f4ee6d40b
Pranay Berry <pranayberry@gmail.com>
2017-07-02 02:07:36 +0530

    Recieve Message

--- FB_ScriptPal/views.py
+++ FB_ScriptPal/views.py
@@ -13,8 +13,21 @@ def home(request):
 	return HttpResponse("Hello world.")
 
 def BotView(request):
-	if request.GET['hub.verify_token'] == '709337517':
-		return HttpResponse(request.GET['hub.challenge'])
-	else:
-		return HttpResponse('Error, invalid token')
+	if request.method == 'GET':
+		if request.GET['hub.verify_token'] == '709337517':
+			return HttpResponse(request.GET['hub.challenge'])
+		else:
+			return HttpResponse('Error, invalid token')
+	if request.method == 'POST':
+		incoming_message = json.loads(request.body.decode('utf-8'))
+		# Facebook recommends going through every entry since they might send
+		# multiple messages in a single call during high load
+		for entry in incoming_message['entry']:
+			for message in entry['messaging']:
+				# Check to make sure the received call is a message call
+				# This might be delivery, optin, postback for other events 
+				if 'message' in message:
+					# Print the message to the terminal
+					pprint(message)
+		return HttpResponse()
 
